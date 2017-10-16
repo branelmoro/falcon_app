9de4113bd6666837686e5b237b0071acb6902c2e
Branel M <branel.m@peopleinteractive.in>
2017-10-16 19:24:39 +0530

    minor change

--- falcon_test.py
+++ falcon_test.py
@@ -1,3 +1,4 @@
+import types
 import app as app_wrapper
 
 controllers = app_wrapper.controllers
@@ -10,14 +11,28 @@ arrControllers = []
 
 # print(getattr(arrControllers))
 
-arrControllers.append(controllers.indexController.index)
-arrControllers.append(controllers.tokenController.index)
-arrControllers.append(controllers.tokenController.resource)
-arrControllers.append(controllers.tokenController.accessScope)
-arrControllers.append(controllers.tokenController.adminUser)
-arrControllers.append(controllers.tokenController.client)
-arrControllers.append(controllers.specialityController.saveSearchSkill)
-arrControllers.append(controllers.specialityController.searchSkillStatus)
+# arrControllers.append(controllers.indexController.index)
+# arrControllers.append(controllers.tokenController.index)
+# arrControllers.append(controllers.tokenController.resource)
+# arrControllers.append(controllers.tokenController.accessScope)
+# arrControllers.append(controllers.tokenController.adminUser)
+# arrControllers.append(controllers.tokenController.client)
+# arrControllers.append(controllers.specialityController.saveSearchSkill)
+# arrControllers.append(controllers.specialityController.searchSkillStatus)
+
+
+def __getControllerClasses(controllers):
+	controller_modules = [controllers.__dict__.get(a) for a in controllers.__dict__ if a != "base_controller" and a.find("__") != 0 and isinstance(controllers.__dict__.get(a), types.ModuleType)]
+	arrControllers = []
+	for i in controller_modules:
+		abc  =  [i.__dict__.get(a) for a in i.__dict__ if a.find("__") != 0 and isinstance(i.__dict__.get(a), type)]
+		arrControllers = arrControllers + abc
+	return arrControllers
+
+arrControllers = __getControllerClasses(controllers)
+
+print(arrControllers)
+
 
 for cntClass in arrControllers:
 	obj = cntClass()
