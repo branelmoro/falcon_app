056c0102c8e3a717d3d44be5dd1c0d2ca485b9a8
branelmoro <branelmoro@gmail.com>
2017-11-27 00:53:42 +0530

    minor change

--- admin_panel/views/sample_view.py
+++ admin_panel/views/sample_view.py
@@ -5,18 +5,11 @@ from library import BASE_HTML
 
 class sample_view(BASE_HTML):
 
-	__text = """
-<html>
-  <head>
-	<title>Spitfire example</title>
-  </head>
-  <body><p>Hello, {name}</p></body>
-</html>
-		"""
+	__text = """ {at}"""
 
 	def _getFormatedText(self):
 
 		# render inner view
 		# self._body["name"] = BASE_HTML.renderView("anotherview", body=self._body["name"], parent=self)
 
-		self._body = self.__text.format(**self._body)
+		return self.__text.format(**self._body)
--- admin_panel/views/test/sample_view.py
+++ admin_panel/views/test/sample_view.py
@@ -5,12 +5,12 @@ from library import BASE_HTML
 
 class sample_view(BASE_HTML):
 
-	__text = """<p>Hello, {name}</p>"""
+	__text = """<p>Hello, {name} {here}</p>"""
 
 	def _getFormatedText(self):
 
 		# render inner view
-		# self._body["name"] = BASE_HTML.renderView("anotherview", body=self._body["name"], parent=self)
+		self._body["here"] = BASE_HTML.renderView("sample_view", body={"at":" here"}, parent=self)
 
 		self._body["name"] = "Branel"
 		return self.__text.format(**self._body)
