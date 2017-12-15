a9776a27693a037196029db1d0481cf52f214dcd
branelmoro <branelmoro@gmail.com>
2017-12-15 08:46:49 +0530

    minor change

--- admin_panel/resources/backend_api/resource_api.py
+++ admin_panel/resources/backend_api/resource_api.py
@@ -4,25 +4,27 @@ from io import BytesIO
 class BACKEND_API(object):
 
 	@classmethod
-	def get(cls, path):
-		return cls.getDataFromApi("GET", path)
+	def get(cls, path, header={}):
+		return cls.__excecute(method="GET", path=path, header=header)
 
 	@classmethod
-	def post(cls, path, data = null):
-		return cls.getDataFromApi("POST", path, data)
+	def post(cls, path, data = null, header={}):
+		return cls.__excecute(method="POST", path=path, data=data, header=header)
 
 	@classmethod
-	def put(cls, path, data = null):
-		return cls.getDataFromApi("PUT", path, data)
+	def put(cls, path, data = null, header={}):
+		return cls.__excecute(method="PUT", path=path, data=data, header=header)
 
 	@classmethod
-	def delete(cls, path, data):
-		return cls.getDataFromApi("DELETE", path, data)
+	def delete(cls, path, data, header={}):
+		return cls.__excecute(method="DELETE", path=path, data=data, header=header)
 
 	@classmethod
-	def __excecute(cls):
+	def __excecute(cls, method, path, data = None, header={}):
 		http_buffer = BytesIO()
 		c = pycurl.Curl()
+		c.setopt(c.CUSTOMREQUEST, method);
+		c.setopt(c.HTTPHEADER, ["Content-Type: application/json"])
 		c.setopt(c.URL, 'http://pycurl.io/')
 		c.setopt(c.WRITEDATA, http_buffer)
 		c.perform()
