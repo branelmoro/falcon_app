8120f5c9619ee49bd7198ef5c23e31ad9469b622
Branel M <branel.m@peopleinteractive.in>
2017-12-19 19:12:23 +0530

    minor change

--- admin_panel/html_test.py
+++ admin_panel/html_test.py
@@ -2,17 +2,41 @@
 # a = BASE_HTML.renderView("test.sample_view")
 # print(a)
 
-import asyncio
+# def coro():
+# 	hello = yield "Hello"
+# 	yield hello
 
+
+# c = coro()
+# print(next(c))
+# print(c.send("World"))
+# exit()
+
+import asyncio
+# [(function1, data1),(function1, data1),(function1, data1)]
+
+
+def runAsync(*args):
+	for detail in args:
+		func = detail[0]
+		data = detail[0]
+	pass
+
+
+
+
+
+@asyncio.coroutine
 def shleepy_time(seconds):
 	print("Shleeping for {s} seconds...".format(s=seconds))
-	yield from asyncio.sleep(seconds)
+	# yield from asyncio.sleep(seconds)
 	# if seconds == 5:
 	# 	raise Exception("custom exception")
 	print("Done Shleeping for {s} seconds...".format(s=seconds))
 
 
 if __name__ == '__main__':
+	shleepy_time(seconds=4)
 	loop = asyncio.get_event_loop()
 
 	# Side note: Apparently, async() will be deprecated in 3.4.4.
@@ -22,8 +46,9 @@ if __name__ == '__main__':
 		asyncio.async(shleepy_time(seconds=3)),
 		asyncio.async(shleepy_time(seconds=7))
 	)
-
+	runAsync()
 	try:
+		print("starting loop")
 		loop.run_until_complete(tasks)
 		print("done event loop")
 	except KeyboardInterrupt as e:
